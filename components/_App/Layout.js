b9004737bb58fb03d7bc6ba2592156868dea6441
Itamar Cohen <you@example.com>
2020-08-02 00:55:10 +0300

    - edit particlesConfig.js - remove semantic link.

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -9,7 +9,6 @@ function Layout({ children }) {
         <HeadContent />
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
         <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Luckiest+Guy&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
         <title>Itamar Cohen</title>
