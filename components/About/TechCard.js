2ddd59937f9840fdaa3a8890382421f117610647
Itamar Cohen <you@example.com>
2020-08-02 13:49:40 +0300

    - add about section - add tech cards + buttons (not functional yet)

---
+++ components/About/TechCard.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTechCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: .75s;
+  position: relative;
+  background: #fff;
+  overflow: hidden;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  padding: 5px;
+  margin: 5px;
+  box-shadow: inset 0 4px 5px 0 rgba(0,0,0,.14), inset 0 1px 10px 0 rgba(0,0,0,.12), inset 0 2px 4px -1px rgba(0,0,0,.3);
+
+  &:hover .img-wrapper {
+  filter: blur(2px);
+  }
+  
+    &:hover .description {
+  opacity: 1
+  }
+
+  .img-wrapper {
+  height: 100%;
+  width: 100%;
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+    img {
+  max-width: 100%;
+  max-height: 100%;
+  transition: .75s ease-in;
+  padding: 10rem;
+  }
+  
+  }
+  
+  .description {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    opacity: 0;
+    background-color: hsla(0,0%,100%,.7);
+    transition: .75s;
+    padding: 5rem;
+    font-weight: bold;
+    user-select: none;
+  }
+`;
+
+const TechCard = ({ imageSrc, text }) => (
+  <StyledTechCard>
+    <div className="img-wrapper">
+      <img alt={text} src={`/devIcons/${imageSrc}`} />
+    </div>
+    <div className="description">
+      <span>{text}</span>
+    </div>
+  </StyledTechCard>
+);
+
+export default TechCard;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,53 @@
 import styled from 'styled-components';
 import useTyped from 'use-typed';
 import Particles from 'react-particles-js';
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
+import useInView from 'react-cool-inview';
 import ParticlesConfig from '../lib/particlesConfig';
-import AnimatedArrow from "../components/AnimatedArrow";
+import AnimatedArrow from '../components/AnimatedArrow';
+import TechCard from '../components/About/TechCard';
+
+const TECH_TYPE = {
+  LANGUAGES: 'LANGUAGES',
+  FRONTEND: 'FRONTEND',
+  BACKEND: 'BACKEND',
+  DATABASE: 'DATABASE',
+  TESTING: 'TESTING',
+  TOOLS: 'TOOLS',
+};
+
+const techCards = [
+  { imageSrc: 'html-5.svg', text: 'HTML', type: TECH_TYPE.LANGUAGES },
+  { imageSrc: 'css-3.svg', text: 'CSS', type: TECH_TYPE.LANGUAGES },
+  { imageSrc: 'javascript.svg', text: 'Javascript', type: TECH_TYPE.LANGUAGES },
+  { imageSrc: 'java.svg', text: 'Java', type: TECH_TYPE.LANGUAGES },
+  { imageSrc: 'python.svg', text: 'Python', type: TECH_TYPE.LANGUAGES },
+  { imageSrc: 'sql.svg', text: 'SQL', type: TECH_TYPE.LANGUAGES },
+  { imageSrc: 'c.svg', text: 'C Language', type: TECH_TYPE.LANGUAGES },
+  { imageSrc: 'react.svg', text: 'React.js', type: TECH_TYPE.FRONTEND },
+  { imageSrc: 'redux.svg', text: 'Redux', type: TECH_TYPE.FRONTEND },
+  { imageSrc: 'scss.svg', text: 'SCSS', type: TECH_TYPE.FRONTEND },
+  { imageSrc: 'css-in-js.png', text: 'CSS in JS', type: TECH_TYPE.FRONTEND },
+  { imageSrc: 'next-js.svg', text: 'Next.js', type: TECH_TYPE.FRONTEND },
+  { imageSrc: 'spring.svg', text: 'Spring Boot', type: TECH_TYPE.BACKEND },
+  { imageSrc: 'nodejs.svg', text: 'Node.js', type: TECH_TYPE.BACKEND },
+  { imageSrc: 'express.png', text: 'Express', type: TECH_TYPE.BACKEND },
+  { imageSrc: 'docker.svg', text: 'Docker', type: TECH_TYPE.BACKEND },
+  { imageSrc: 'mysql.svg', text: 'MySQL', type: TECH_TYPE.DATABASE },
+  { imageSrc: 'mongodb.png', text: 'Mongo DB', type: TECH_TYPE.DATABASE },
+  { imageSrc: 'vertica.svg', text: 'Vertica', type: TECH_TYPE.DATABASE },
+  { imageSrc: 'firebase.svg', text: 'FireStore', type: TECH_TYPE.DATABASE },
+  { imageSrc: 'selenium.svg', text: 'Selenium', type: TECH_TYPE.TESTING },
+  { imageSrc: 'junit.png', text: 'Junit', type: TECH_TYPE.TESTING },
+  { imageSrc: 'jest.svg', text: 'Jest', type: TECH_TYPE.TESTING },
+  { imageSrc: 'rest-assured.png', text: 'Rest Assured', type: TECH_TYPE.TESTING },
+  { imageSrc: 'puppeteer.svg', text: 'Puppeteer', type: TECH_TYPE.TESTING },
+  { imageSrc: 'git.svg', text: 'Git', type: TECH_TYPE.TOOLS },
+  { imageSrc: 'jenkins.webp', text: 'Jenkins', type: TECH_TYPE.TOOLS },
+  { imageSrc: 'jira.svg', text: 'Jira', type: TECH_TYPE.TOOLS },
+  { imageSrc: 'npm.svg', text: 'NPM', type: TECH_TYPE.TOOLS },
+  { imageSrc: 'eslint.svg', text: 'ESlint', type: TECH_TYPE.TOOLS },
+];
 
 const Container = styled.section`
   height: 100vh;
@@ -22,6 +66,17 @@ const Container = styled.section`
       z-index: 0;
     }
   }
+`;
+
+const Containerr = styled.section`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  padding: 100rem 15rem 0 15rem;
+  background:  url("/RetinaWoodOver.webp");
+  text-align: center;
+  display: flex;
+  justify-content: center;
 `;
 
 const FlexContainer = styled.div`
@@ -75,6 +130,61 @@ const FloatingAvatar = styled.div`
 }
 `;
 
+const AboutTitle = styled.h2`
+font-size: 30rem;
+color: #2b2b2b;
+`;
+
+const UnderLine = styled.div`
+    background: #823b00;
+    height: 4rem;
+    border-radius: 20%;
+    margin: 2rem auto 0 auto;
+    width: 60rem;
+`;
+
+const AboutText = styled.div`
+font-size: 16rem;
+color: ${({ theme }) => theme.font.black};
+white-space: pre-wrap;
+text-align: left;
+`;
+
+const AboutContainer = styled.div`
+  display: grid;
+  grid-template-columns: 0.9fr auto;
+  grid-template-rows: 1fr auto;
+  max-width: 1200rem;
+  gap: 6rem;
+  margin-top: 100rem;
+`;
+
+const Links = styled.div`
+display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: calc((100% - 63px) - 19.92px);
+    position: relative;
+    bottom: 10px;
+`;
+
+const TechText = styled.div`
+font-size: 16rem;
+color: ${({ theme }) => theme.font.black};
+white-space: pre-wrap;
+text-align: center;
+`;
+
+const TechButtons = styled.div`
+display: flex;
+justify-content: center
+`;
+
+const TechCardContainer = styled.div`
+display: flex;
+flex-wrap: wrap;
+`;
+
 const Home = () => {
   const typedRef = useRef(null);
   useTyped(typedRef, {
@@ -89,23 +199,89 @@ const Home = () => {
     smartBackspace: true,
   });
 
+  const {
+    ref: viewRef, inView, scrollDirection, entry, observe, unobserve,
+  } = useInView(
+    {
+      threshold: 0.95,
+      onChange: ({
+        inView, scrollDirection, entry, observe, unobserve,
+      }) => {
+        console.log('change', {
+          inView, scrollDirection, entry, observe, unobserve,
+        });
+      },
+      onEnter: ({
+        scrollDirection, entry, observe, unobserve,
+      }) => {
+        console.log('enter', {
+          scrollDirection, entry, observe, unobserve,
+        });
+      },
+      onLeave: ({
+        scrollDirection, entry, observe, unobserve,
+      }) => {
+        console.log('leave', {
+          scrollDirection, entry, observe, unobserve,
+        });
+      },
+    },
+  );
+
+  const techCardComponents = useMemo(() => techCards.map((techObject) => <TechCard key={techObject.imageSrc} imageSrc={techObject.imageSrc} text={techObject.text} />), []);
+
   return (
-    <Container>
-      <Particles className="particles" params={ParticlesConfig} />
-      <FlexContainer>
-        <div>
+    <>
+      <Container>
+        <Particles className="particles" params={ParticlesConfig} />
+        <FlexContainer>
           <FloatingAvatar>
-            <img src="/avatar.svg" alt="Skytsunami" />
+            <img src="/avatar.svg" alt="Itamar Avatar" />
           </FloatingAvatar>
-          <h1>
-            Hello, I'm
-            <ColoredText color="#fca92e"> Itamar Cohen </ColoredText>
-          </h1>
-          <span className="typed" ref={typedRef} />
+          <div>
+            <h1>
+              Hello, I'm
+              <ColoredText> Itamar Cohen </ColoredText>
+            </h1>
+            <span className="typed" ref={typedRef} />
+          </div>
+        </FlexContainer>
+        <AnimatedArrow />
+      </Container>
+      <Containerr ref={viewRef}>
+        <div>
+          <AboutTitle> About </AboutTitle>
+          <UnderLine />
+          <AboutContainer>
+            <AboutText>
+              I’m Itamar, a Full Stack Web Developer at Taboola and Bachelor of Electrical Engineering at Tel Aviv University.
+              I have a strong passion for web development and design which influences my professional work, as well as my side projects.
+              I'm able to work in the 3 major areas of web development: frontend, backend, and the database.
+            </AboutText>
+            <Links>
+              <div> adsd</div>
+              <div> adsd</div>
+              <div> adsd</div>
+              <div> adsd</div>
+            </Links>
+            <div>
+              <h2> Skill Set </h2>
+              <TechButtons>
+                <button>All</button>
+                <button>Languages</button>
+                <button>Frontend</button>
+                <button>Backend</button>
+                <button>Tools</button>
+              </TechButtons>
+              <TechCardContainer>
+                {techCardComponents}
+              </TechCardContainer>
+            </div>
+
+          </AboutContainer>
         </div>
-      </FlexContainer>
-      <AnimatedArrow />
-    </Container>
+      </Containerr>
+    </>
   );
 };
 
